1a7be7b9743c4c8d884a54e6fa530281f412d238
dinuka bk <dinukabk@gmail.com>
2023-01-03 13:39:50 +0530

    change routes to controller class

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [TodoController::class, 'index']);
 
-Route::get('/create',function(){
-    return view('create');
-});
-Route::get('/details',function(){
-    return view('details');
-});
-Route::get('/edit',function(){
-    return view('edit');
-});
+Route::get('create', [TodoController::class, 'create']);
+
+Route::get('details', [TodoController::class, 'details']);
+Route::get('edit', [TodoController::class, 'edit']);
+Route::post('update', [TodoController::class, 'update']);
+
+Route::get('delete', [TodoController::class, 'delete']);
